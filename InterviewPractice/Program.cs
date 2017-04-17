7e47a2083f9e619441ff0f4d980346febffd4930
Christopher Hunter <chunter791@gmail.com>
2017-04-17 14:01:44 +0100

    Added methods for generating a Fibonacci sequence with user input to select how many numbers it will produce. Both iterative and recursive solutions provided.

--- InterviewPractice/Program.cs
+++ InterviewPractice/Program.cs
@@ -10,7 +10,64 @@ namespace InterviewPractice
     {
         static void Main(string[] args)
         {
+            int number; 
+
+            Console.WriteLine("-------- Iterative Fibonacci --------");
+            Console.WriteLine("Enter the number upto which Fibonacci series to print: ");
+            number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Fibonacci series upto " + number + " numbers:");
+            for (int i = 1; i <= number; i++)
+            {
+                Console.Write(FibonacciIteration(i) + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("-------- Recursive Fibonacci --------");
+            Console.WriteLine("Enter the number upto which Fibonacci series to print: ");
+            number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" Fibonacci series upto " + number + " numbers:");
+            for (int i = 1; i <= number; i++)
+            {
+                Console.Write(FibonacciRecursive(i) + " ");
+            }
+            Console.WriteLine();
 
         }
+
+        // Fibonacci Sequence
+        static int FibonacciIteration(int number)
+        {
+            if (number == 1 || number == 2)
+            {
+                return 1;
+            }
+
+            int fibo1 = 1, fibo2 = 1, fibonacci = 1;
+
+            for (int i = 3; i <= number; i++)
+            {
+                fibonacci = fibo1 + fibo2;
+                fibo1 = fibo2;
+                fibo2 = fibonacci;
+            }
+
+            return fibonacci;
+        }
+
+        static int FibonacciRecursive(int number)
+        {
+            if (number == 1 || number == 2)
+            {
+                return 1;
+            }
+
+            return FibonacciRecursive(number - 1) + FibonacciRecursive(number - 2);
+        }
+        // Check if a string is a palindrome
+
+        // Find a character within an array
+
     }
 }
